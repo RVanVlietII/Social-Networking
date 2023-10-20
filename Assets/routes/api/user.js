075d5fbd35149ee72da4287ca99ef3333eb5d3f0
RVanVlietII <rcvanvlietii@gmail.com>
2023-10-20 17:13:43 -0400

    updated controllers for user and thoughts

--- Assets/routes/api/user.js
+++ Assets/routes/api/user.js
@@ -8,7 +8,7 @@ const {
   removeThoughts,
   addFriend,
   removeFriend,
-} = require('../../controllers/studentController');
+} = require('../../controllers/thoughtController');
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
